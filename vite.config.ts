b8f03dd8095e980735ccad1468416214aea8d909
Marwan Magdy <marwanmagdii@example.com>
2026-08-02 19:49:03 +0300

    chore: add gh-pages deploy script and vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/bue-booking-app/',
   plugins: [react()],
   resolve: {
     alias: {
